3f9caf399af60be2b461b9c8151e249eb0623873
folmer <f-olli@hotmail.com>
2016-10-26 09:43:26 +0200

    Pseudo code template created

---
+++ setup.py
@@ -0,0 +1,5 @@
+def main():
+    TTP = new TTP(params...)
+    if not_generated or force_generate:
+        TTP.generate_master()
+    user1 = new User(person, attributes)
---
+++ test.py
@@ -0,0 +1,7 @@
+import pebel as pb
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
